392d9d377c223e792689de9b74fbfeeca0021424
jabishvili27 <jabishvili27@gmail.com>
2021-11-16 18:55:25 +0400

    angular-task1-task2

---
+++ practice/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const person = {
+    name: toma,
+    age: 20,
+    sur: jaa
+};
---
+++ practice/index.ts
@@ -0,0 +1,13 @@
+
+
+interface person1{
+    fullname:string;
+    age:number;
+    sur:string;
+}
+
+const person:person1={
+    name:toma,
+    age:20,
+    sur:jaa
+}
